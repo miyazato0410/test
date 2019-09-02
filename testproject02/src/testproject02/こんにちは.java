522aa7a646192480a9c539f4e7e3929fa88047e0
miyazato0410 <larc.4646@icloud.com>
2019-09-02 21:37:54 +0900

    test01

---
+++ testproject02/src/testproject02/こんにちは.java
@@ -0,0 +1,14 @@
+package testproject02;
+
+public class こんにちは {
+
+	public static void main(String[] args){
+
+    int i=10;
+
+    do{
+    	System.out.println(i);
+    	i-=3;
+    }while(i>=1);
+	}
+}
