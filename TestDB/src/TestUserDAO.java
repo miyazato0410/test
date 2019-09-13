b74a1f821466308feabce5a28cff4cd1fc47ecad
miyazato0410 <larc.4646@icloud.com>
2019-09-13 21:48:29 +0900

    0913

---
+++ TestDB/src/TestUserDAO.java
@@ -0,0 +1,11 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TestUserDAO {
+
+	String name="";
+	String password="";
+
+ここから入力
